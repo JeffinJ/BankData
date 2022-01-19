99543cdecafe854cb9f1a7712ff9752c1775ccc2
Jeffin <jeffinj787@gmail.com>
2022-01-19 23:59:19 +0530

    Added Postgres DB from  heroku

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,10 @@ import Pool from "pg";
 import { DB_Config } from "./config";
 
 export const pool = new Pool.Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
+    user: "gxuxumkgsoqxeq",
+    password: "8baa90a4a4ea63aba136a10cc4ef9581cccfcd774ffa8363e62d9a3ff3af3fe1",
+    host: "ec2-3-210-124-68.compute-1.amazonaws.com",
     port: 5432,
-    database: "indian_banks"
+    database: "d58ge8etb5oo72"
 });
 
